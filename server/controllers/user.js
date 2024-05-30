736fd1810d9f7a2cd0c0958125d4f51e9bca4308
36Tri <trit6013@gmail.com>
2024-05-30 15:53:35 +0700

    #3 Register & Login

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -11,15 +11,42 @@ const register = asyncHandler(async(req, res) => {
             message: 'Missing Inputs',
         })
     }
-     
 
-    const response = await User.create(req.body)
-    return res.status(200).json({
-        sucess: response ? true : false,
-        response
-    })          
+    const user = await User.findOne({email})
+    if (user) throw new Error('User existed')
+    else {
+        const newUser = await User.create(req.body)
+        return res.status(200).json({
+            sucess: newUser ? true : false,
+            mess: newUser ? 'Register is Successful. Please Login!!!!!' : 'Something went wrong'
+        })
+    }         
+})
+
+const login = asyncHandler(async(req, res) => {
+    const {email, password} = req.body
+
+    console.log(req)
+
+    if (!email || !password) 
+        return res.status(400).json({
+            sucess: false,
+            message: 'Missing Inputs',
+        })
+
+    const response = await User.findOne({email})
+    if (response && await response.isCorrectPassword(password)){
+        const { password, role, ...userData } = response.toObject()
+        return res.status(200).json({
+            sucess: true,
+            userData
+        })
+    } else {
+        throw new Error('Invalid password!!!')
+    }
 })
 
 module.exports = {
-    register
+    register,
+    login
 }
